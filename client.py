62e09e0e59ff07bddf9045df2b59e497e34efad3
YBDYY <Deividuks2007@gmail.com>
2025-02-24 14:29:32 +0200

    Fix threading logic for receiving and sending messages in client.py

--- client.py
+++ client.py
@@ -10,21 +10,14 @@ def listen_for_messages(peer_socket):
             if message:
                 print(f"Received message: {message}")
         except:
+            print("Disconnected from server.")
             break
 
 
 def send_message(peer_socket):
     while True:
         try:
-            message = peer_socket.recv(1024).decode('utf-8')
-            if message:
-                print(f"Received message: {message}")
-        except:
-            break
-def send_message(peer_socket):
-    while True:
-        try:
-            message = input("You:")
+            message = input(": ")
             if message:
                 peer_socket.send(message.encode('utf-8'))
         except:
@@ -33,10 +26,10 @@ def send_message(peer_socket):
 def connect_to_peer(peer_address):
     peer_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     peer_socket.connect(peer_address)
-    print(f"Connected to peer at {peer_address}")
+    print(f"Connected to chat at {peer_address}")
 
-    threading.Thread(target=listen_for_messages, args=(peer_socket,)).start()
-    threading.Thread(target=send_message, args=(peer_socket,)).start()
+    threading.Thread(target=listen_for_messages, args=(peer_socket,), daemon=True).start()
+    send_message(peer_socket)
 
 
 if __name__ == "__main__":
