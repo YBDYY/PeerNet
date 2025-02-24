af3cbe6c48d4a16477b14287f5c38ff5c7bbfbdc
YBDYY <Deividuks2007@gmail.com>
2025-02-24 14:30:43 +0200

    Enabled multiple client connections with broadcast functionality in server.py

--- server.py
+++ server.py
@@ -11,6 +11,37 @@ def listen_for_messages(peer_socket):
             print("Connection lost")
             break
 
+
+clients=[]
+
+def broadcast(message, sender_socket):
+    
+    for client in clients[:]:  
+        if client != sender_socket:
+            try:
+                client.send(message.encode('utf-8'))
+            except:
+                clients.remove(client)  
+
+
+
+def handle_client(client_socket, client_address):
+    print(f"New Connection from {client_address}")
+    
+    clients.append(client_socket)
+
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if message:
+                print(f"Received from {client_address}: {message}")
+                broadcast(message, client_socket)  
+        except:
+            print(f"Client {client_address} disconnected.")
+            clients.remove(client_socket)  
+            client_socket.close()
+            break
+
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('localhost',12345))
